004f182a68a4c2dd52b727c61fada02eceb2db4f
huifeng_du <huifeng_du@foxitsoftware.com>
2020-11-24 12:01:12 +0800

    Add:add sreen all function

--- QScreenShot/mainwindow.cpp
+++ QScreenShot/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QWindow>
+#include <QLabel>
+#include <qfiledialog.h>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -22,5 +26,21 @@ void MainWindow::InitSlots()
 
 void MainWindow::Slots_ScreenAll()
 {
-    QMessageBox::about(this,"标题","文字");
+    QWindow * myScreenWindow = windowHandle();
+    QScreen * myScreen =myScreenWindow->screen();
+    QPixmap pixmap = myScreen->grabWindow(0);
+    QString strFolderPath = QFileDialog::getExistingDirectory(this,"Choose To Save File","/");
+    QString strSavePath = "";
+    if(strFolderPath.isEmpty())
+    {
+        return ;
+    }
+    else {
+        strSavePath = strFolderPath +"/ScreenAll.jpg";
+        bool bSave = pixmap.save(strSavePath);
+        if(bSave)
+        {
+            QMessageBox::information(this,"Save","Path:"+strSavePath,QMessageBox::Yes);
+        }
+    }
 }
